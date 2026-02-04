589982660c17b442eb73b8b8e88baabb3ede686c
keheliyavimu <keheliyavimu@gmail.com>
2026-02-04 21:02:22 +0530

    Initial commit - BHP project

---
+++ server/server.py
@@ -0,0 +1,76 @@
+# import app
+# from flask import Flask, request, jsonify
+# import util
+#
+# app = Flask(__name__)
+#
+# # solves not taking location error
+# util.load_saved_artifacts()
+#
+# @app.route('/get_location_names')
+# def get_location_names():
+#     response = jsonify({
+#         'locations': util.get_location_names()
+#     })
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#
+#     return response
+#
+# @app.route('/predict_home_price', methods=['POST'])
+# def predict_home_price():
+#     total_sqft = float(request.form['total_sqft'])
+#     location = request.form['location']
+#     bhk = int(request.form['bhk'])
+#     bath = int(request.form['bath'])
+#
+#     response = jsonify({
+#         'estimate_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+#     })
+#
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#
+#     return response
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import util
+
+app = Flask(__name__)
+CORS(app)   # allow frontend (app.html) to call backend
+
+# Load model + columns once when server starts
+util.load_saved_artifacts()
+
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    sqft = float(request.form['sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    estimated_price = util.get_estimated_price(location, sqft, bhk, bath)
+
+    response = jsonify({
+        'estimated_price': estimated_price
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+if __name__ == "__main__":
+    print("Starting Flask server...")
+    app.run(debug=True)
